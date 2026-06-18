e9a1f96982bd1cbea8da09699f951f0fc7a92fe5
Pankaj Sharma <#anishak47>
2026-06-18 10:44:15 +0530

    new django project

---
+++ myproject/myapp/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.core.paginator import Paginator
+from .models import Product
+
+def product_list(request):
+    search=request.GET.get('search')
+    if search:
+        product_list=Product.objects.filter(name__icontains=search)
+    else:
+        product_list=Product.objects.all()
+
+    paginator=Paginator(product_list,5)  
+    page_number=request.GET.get('page')
+    page_obj=paginator.get_page(page_number)
+    return render(request, 'index.html', {'page_obj': page_obj, 'search':search}) 
+
+# Create your views here.
