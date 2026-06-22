21544c7088dede05b62644be0a1732b84640d981
Pankaj Sharma <#anishak47>
2026-06-22 11:21:35 +0530

    enable debug

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 SECRET_KEY = config('SECRET_KEY')
 
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = [
 
